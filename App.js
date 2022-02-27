cee51b94b748595182c02c6ea8a98f91c91a1694
SimonBucko <bucino.36@gmail.com>
2022-02-27 21:00:01 +0100

    added custom fonts

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import Home from "./screens/home"
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'roboto-mono': 'https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap',
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-
-      </View>
-    </View>
+    <Home />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/home.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { StyleSheet, View, Text } from "react-native"
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Home Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    },
+    text: {
+        fontFamily: "roboto-mono"
+    }
+})
