288c0739892b7c413cdbc70833e33516cbe01b31
SimonBucko <bucino.36@gmail.com>
2022-03-06 18:54:03 +0100

    added global styles

--- screens/home.js
+++ screens/home.js
@@ -1,19 +1,11 @@
 import React from "react"
-import { StyleSheet, View, Text } from "react-native"
+import { View, Text } from "react-native"
+import { globalStyles } from "../styles/global"
 
 export default function Home() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Home Screen</Text>
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.text}>Home Screen</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-    },
-    text: {
-        fontFamily: "roboto-regular"
-    }
-})
